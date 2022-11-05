fbea2bc4c95a33f923119a428138b6e23bf5f4a1
Grabos179 <grabowski.piotr020@gmail.com>
2022-11-05 11:18:19 +0100

    zad3(dbset)

---
+++ FilmBD/FilmBD/FilmContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmBD
+{
+    public class FilmContext : DbContext
+    {
+        protected override void OnComfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cs = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FilmDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(cs);
+        }
+
+
+    }
+}
--- FilmBD/FilmBD/Startup.cs
+++ FilmBD/FilmBD/Startup.cs
@@ -50,7 +50,7 @@ namespace FilmBD
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Film}/{action=Index}/{id?}");
             });
         }
     }
